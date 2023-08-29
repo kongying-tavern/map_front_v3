f0b0ffafa2062a95cedbaebaabedbc9aaf0a7da7
Alex Fang <deeno@qq.com>
2023-08-30 06:26:31 +0800

    chore: change tips

--- src/api/operation.js
+++ src/api/operation.js
@@ -28,14 +28,14 @@ export const opNonOfficialMsgShow = () => {
       html: true,
       message: `
           <div class="text-bold">
-            <div class="text-red">本地图由 <span class="text-teal">空荧酒馆</span> 提供，但您当前可能正使用非官方软件访问该页面。</div>
-            <div class="text-red">非官方应用访问此地图会产生色差，为正常现象。</div>
+            <div class="text-red">本地图为 <span class="text-teal">空荧酒馆</span> 提供的网页版本。</div>
+            <div class="text-red">您当前使用的并非官方出品，使用中出现的一切问题我方概不负责。</div>
             <div>&nbsp;</div>
-            <div>官方声明：</div>
-            <div>网页版地址：<a target="_blank" href="https://yuanshen.site/">https://yuanshen.site/</a></div>
-            <div>PC客户端下载：<a target="_blank" href="https://yuanshen.site/docs/download-client">点击此处</a></div>
+            <div><span class="text-teal">空荧酒馆</span> 暂未提供手机客户端</div>
+            <div>手机请使用官网：<a target="_blank" href="https://yuanshen.site">yuanshen.site</a></div>
+            <div>电脑客户端下载：<a target="_blank" href="https://yuanshen.site/docs/download-client">点此下载</a></div>
             <div>&nbsp;</div>
-            <div>目前 <span class="text-teal">空荧酒馆</span> 暂未提供手机版，<br>可使用网页版，或 <a target="_blank" href="https://yuanshen.site/docs/join">加入开发</a></div>
+            <div>急缺美术，如果您是 UI/UX 设计师，可以 <a target="_blank" href="https://yuanshen.site/docs/join">加入开发</a>，以推进移动端开发进度</div>
           </div>
         `,
       persistent: true,
