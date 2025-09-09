7282cb642202577070069313cda409f9dd193e80
Alex Fang <deeno@qq.com>
2025-09-10 03:21:33 +0800

    build: 调整网页调试环境配置

--- src/service/config_request.js
+++ src/service/config_request.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 
 function fetch_config() {
+  const url = !import.meta.env.DEV
+    ? `https://assets.yuanshen.site/web-map.json?r=${Math.random()}`
+    : `https://assets.yuanshen.site/web-map-preview.json?r=${Math.random()}`;
   return axios
-    .get(`https://assets.yuanshen.site/web-map.json?r=${Math.random()}`)
+    .get(url)
     .then((res) => res.data || {})
     .catch(() => ({}));
 }
