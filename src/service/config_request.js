4e45bdea0af59ecb8c63c277d31a61df80ad8e98
Alex Fang <deeno@qq.com>
2026-04-17 22:54:06 +0800

    chore: 调整配置路径

--- src/service/config_request.js
+++ src/service/config_request.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 function fetch_config() {
   const url = !import.meta.env.DEV
-    ? `https://assets.yuanshen.site/web-map.json?r=${Math.random()}`
-    : `https://assets.yuanshen.site/web-map-preview.json?r=${Math.random()}`;
+    ? `https://assets.yuanshen.site/webapp.json?r=${Math.random()}`
+    : `https://assets.yuanshen.site/webapp-preview.json?r=${Math.random()}`;
   return axios
     .get(url)
     .then((res) => res.data || {})
